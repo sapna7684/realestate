ab6e7edeafeeaca22df10d6f08866fd799a0088c
sapna_sharma <sapna7864@gmail.com>
2022-05-26 16:36:13 +0530

    progress issue resolved

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Router from 'next/router'
 import Head from 'next/head'
-import NProgress from 'nprogress'
+import nProgress from "nprogress";
+import "../styles/globals.css";
+import "../styles/nprogress.css";
 import { Layout } from '../components/Layout'
 
-NProgress.configure({showSpinner: false})
-Router.events.on('routeChangeStart', () => {
-  NProgress.start();
-});
-Router.events.on('routerChangeError', () => {
-  NProgress.done();
-})
-Router.events.on('routerChangeComplete', () => {
-  NProgress.done();
-})
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
